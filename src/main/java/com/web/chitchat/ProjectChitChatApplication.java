4c34b9a07b06f2d367f103f0a43d7fd6c799c5ce
Saransh Tyagi <tyagisaransh90@gmail.com>
2023-04-11 21:00:06 +0530

    add necessary dependencies and complete the setup

---
+++ src/main/java/com/web/chitchat/ProjectChitChatApplication.java
@@ -0,0 +1,13 @@
+package com.web.chitchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectChitChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectChitChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/chitchat/ProjectChitChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.chitchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectChitChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
